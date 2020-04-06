f23ed523d4092c0893911c7c427b76f86f7d926c
thiagodevx <thiago.dev.x@gmail.com>
2020-04-06 19:43:13 -0300

    primeiros updates do desafio

--- src/app.js
+++ src/app.js
@@ -1,33 +1,44 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express')
+const cors = require('cors')
+const { uuid } = require('uuidv4')
 
-// const { uuid } = require("uuidv4");
+const app = express()
 
-const app = express();
+app.use(express.json())
+app.use(cors())
 
-app.use(express.json());
-app.use(cors());
+const repositories = []
 
-const repositories = [];
+app.get('/repositories', (request, response) => {
+  response.json(repositories)
+})
 
-app.get("/repositories", (request, response) => {
-  // TODO
-});
+app.post('/repositories', (request, response) => {
+  const repository = { id: uuid(), ...request.body, likes: 0 }
+  response.json(repository)
+})
 
-app.post("/repositories", (request, response) => {
-  // TODO
-});
+app.put('/repositories/:id', (request, response) => {
+  const { id } = request.params
+  const { titulo, url, techs } = request.body
+  const index = repositories.findIndex(repository => repository.id == id)
+  const repository = { ...repositories[index], titulo, url, techs }
+  repositories[index] = repository
+})
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
-});
+app.delete('/repositories/:id', (request, response) => {
+  const { id } = request.params
+  const index = repositories.findIndex(repository => repository.id == id)
+  repositories.splice(index, 1)
+  response.status(204)
+})
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
-});
+app.post('/repositories/:id/like', (request, response) => {
+  const { id } = request.params
+  const index = repositories.findIndex(repository => repository.id == id)
+  const repository = repositories[index]
+  repository.like++
+  response.json(repository)
+})
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
-});
-
-module.exports = app;
+module.exports = app
