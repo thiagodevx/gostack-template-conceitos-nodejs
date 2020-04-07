cc0b3341ca55808d218fc5f53eb386667a577549
thiagodevx <thiago.dev.x@gmail.com>
2020-04-07 16:16:39 -0300

    updates para o desafio com os testes rodando

--- src/app.js
+++ src/app.js
@@ -15,29 +15,47 @@ app.get('/repositories', (request, response) => {
 
 app.post('/repositories', (request, response) => {
   const repository = { id: uuid(), ...request.body, likes: 0 }
+  repositories.push(repository)
   response.json(repository)
 })
 
 app.put('/repositories/:id', (request, response) => {
   const { id } = request.params
-  const { titulo, url, techs } = request.body
+  const { title, url, techs } = request.body
   const index = repositories.findIndex(repository => repository.id == id)
-  const repository = { ...repositories[index], titulo, url, techs }
+  if (index == -1) {
+    response.status(400)
+    response.end()
+    return
+  }
+  const repository = { ...repositories[index], title, url, techs }
   repositories[index] = repository
+  response.json(repository)
 })
 
 app.delete('/repositories/:id', (request, response) => {
   const { id } = request.params
   const index = repositories.findIndex(repository => repository.id == id)
+  if (index == -1) {
+    response.status(400)
+    return response.json({})
+  }
   repositories.splice(index, 1)
   response.status(204)
+  return response.json({})
 })
 
 app.post('/repositories/:id/like', (request, response) => {
   const { id } = request.params
   const index = repositories.findIndex(repository => repository.id == id)
+  if (index == -1) {
+    response.status(400)
+    response.end()
+    return
+  }
   const repository = repositories[index]
-  repository.like++
+  repository.likes++
+  console.log(repository.like)
   response.json(repository)
 })
 
